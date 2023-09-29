983bf0d7d51caa35430f8fd748dc10b21d7b4e20
Ivan <ionufriev95@gmail.com>
2023-09-29 19:40:30 +0600

    Gamepush & MirraMetrika imported

---
+++ Assets/Codebase/Infrastructure/ServicesManagment/Ads/GamePushAdService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assets.Codebase.Infrastructure.ServicesManagment.Ads
+{
+    internal class GamePushAdService : IAdsService
+    {
+        public void SetAdsStatus(bool adsEnabled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowFullscreen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowRewarded()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
