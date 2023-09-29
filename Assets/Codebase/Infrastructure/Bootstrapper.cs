19a38518be2ba1934fb8c2c59fe09939dbc75060
Ivan <ionufriev95@gmail.com>
2023-09-29 21:41:07 +0600

    Game structure creator

--- Assets/Codebase/Infrastructure/Bootstrapper.cs
+++ Assets/Codebase/Infrastructure/Bootstrapper.cs
@@ -1,14 +1,8 @@
+using Assets.Codebase.Infrastructure.Initialization;
 using Assets.Codebase.Infrastructure.ServicesManagment;
-using Assets.Codebase.Infrastructure.ServicesManagment.Ads;
-using Assets.Codebase.Infrastructure.ServicesManagment.Assets;
 using Assets.Codebase.Infrastructure.ServicesManagment.ViewCreation;
-using Assets.Codebase.Models.Gameplay;
-using Assets.Codebase.Models.Progress;
-using Assets.Codebase.Presenters.Base;
-using Assets.Codebase.Presenters.Example;
 using Assets.Codebase.Utils.GOComponents;
 using Assets.Codebase.Views.Base;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Codebase.Infrastructure
@@ -18,51 +12,13 @@ namespace Assets.Codebase.Infrastructure
     {
         [SerializeField] private RectTransform _uiRoot;
 
-        private IProgressModel _progressModel;
-        private IGameplayModel _gameplayModel;
-        private List<BasePresenter> _presenters;
-
         private void Awake()
         {
-            InitMVPStructure();
-
-            RegisterServices();
+            // Create game structure
+            GameStructure structure = new GameStructure(_uiRoot);
 
             // Start the game
             ServiceLocator.Container.Single<IViewCreatorService>().CreateView(ViewId.ExampleView);
-        }
-
-        private void InitMVPStructure()
-        {
-            CreateModels();
-            CreatePresenters();
-        }
-
-        private void CreateModels()
-        {
-            _progressModel = new LocalProgressModel();
-            _gameplayModel = new GameplayModel();
-        }
-
-        private void CreatePresenters()
-        {
-            // Create presenter for each view
-            _presenters = new List<BasePresenter>();
-            _presenters.Add(new ExamplePresenter());
-
-            foreach (var presenter in _presenters)
-            {
-                presenter.SetupModels(_progressModel, _gameplayModel);
-            }
-        }
-
-        private void RegisterServices()
-        {
-            var services = ServiceLocator.Container;
-
-            services.RegisterSingle<IAssetProvider>(new AssetProvider());
-            services.RegisterSingle<IViewCreatorService>(new ViewCreatorService(services.Single<IAssetProvider>(), _presenters, _uiRoot));
-            services.RegisterSingle<IAdsService>(new GamePushAdService());
         }
     }
 }
--- Assets/Codebase/Infrastructure/Initialization/GameStructure.cs
+++ Assets/Codebase/Infrastructure/Initialization/GameStructure.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using Assets.Codebase.Infrastructure.ServicesManagment.Ads;
+using Assets.Codebase.Infrastructure.ServicesManagment.Assets;
+using Assets.Codebase.Infrastructure.ServicesManagment.ViewCreation;
+using Assets.Codebase.Infrastructure.ServicesManagment;
+using Assets.Codebase.Models.Gameplay;
+using Assets.Codebase.Models.Progress;
+using Assets.Codebase.Presenters.Base;
+using Assets.Codebase.Presenters.Example;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Codebase.Infrastructure.Initialization
 {
+    /// <summary>
+    /// Responsible for creation of game structure.
+    /// </summary>
     public class GameStructure
     {
+        public static bool IsGameInitialized = false;
+
+        // Needed from outside
+        private RectTransform _uiRoot;
+
+        // Created inside
+        private IProgressModel _progressModel;
+        private IGameplayModel _gameplayModel;
+        private List<BasePresenter> _presenters;
+
+        public GameStructure(RectTransform uiRoot)
+        {
+            _uiRoot = uiRoot;
+
+            InitMVPStructure();
+            RegisterServices();
+
+            IsGameInitialized = true;
+        }
+
+        private void InitMVPStructure()
+        {
+            CreateModels();
+            CreatePresenters();
+        }
+
+        private void CreateModels()
+        {
+            _progressModel = new LocalProgressModel();
+            _gameplayModel = new GameplayModel();
+        }
+
+        private void CreatePresenters()
+        {
+            // Create presenter for each view
+            _presenters = new List<BasePresenter>();
+            _presenters.Add(new ExamplePresenter());
+
+            foreach (var presenter in _presenters)
+            {
+                presenter.SetupModels(_progressModel, _gameplayModel);
+            }
+        }
+
+        /// <summary>
+        /// Registering all game services.
+        /// </summary>
+        private void RegisterServices()
+        {
+            var services = ServiceLocator.Container;
+
+            services.RegisterSingle<IAssetProvider>(new AssetProvider());
+            services.RegisterSingle<IViewCreatorService>(new ViewCreatorService(services.Single<IAssetProvider>(), _presenters, _uiRoot));
+            services.RegisterSingle<IAdsService>(new GamePushAdService());
+        }
     }
 }
