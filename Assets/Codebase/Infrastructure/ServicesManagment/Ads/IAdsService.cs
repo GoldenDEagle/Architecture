3d495638378102796239af85190b97845619d8af
Ivan <ionufriev95@gmail.com>
2023-09-29 18:00:52 +0600

    Ads interface

---
+++ Assets/Codebase/Infrastructure/ServicesManagment/Ads/IAdsService.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Codebase.Infrastructure.ServicesManagment.Ads
+{
+    /// <summary>
+    /// Managing ads.
+    /// </summary>
+    public interface IAdsService : IService
+    {
+        public void ShowRewarded();
+        public void ShowFullscreen();
+
+        /// <summary>
+        /// Enables or disables ads.
+        /// </summary>
+        /// <param name="adsEnabled"></param>
+        public void SetAdsStatus(bool adsEnabled);
+    }
+}
